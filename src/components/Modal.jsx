52bd7bf6d39235e1dd200196e5eb8d534e6106e8
thki91 <thealein1991@gmail.com>
2023-11-11 13:37:33 +0100

    fix close button in modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -32,21 +32,17 @@ const Modal = ({ handleClose, show, children }) => {
       aria-modal="true"
     >
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              {children}
-            </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button
-                type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              <div
+                className="absolute right-0 top-0 py-2.5 px-3.5 text-lg font-semibold cursor-pointer hover:text-stone-500 transition"
                 onClick={handleClose}
               >
-                Schließen
-              </button>
+                &#10005;
+              </div>
+              {children}
             </div>
           </div>
         </div>
